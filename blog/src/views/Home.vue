2f3e4a95e1afe4debe1fdbab6da222c7016026ac
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2021-10-10 20:27:09 +0200

    Added detail view

--- blog/src/views/Home.vue
+++ blog/src/views/Home.vue
@@ -3,40 +3,25 @@
     <h1>Home</h1>
     <div v-if="error">{{ error }}</div>
     <div v-if="posts.length">
-      <PostList :posts="posts"/>
+      <PostList :posts="posts" />
     </div>
     <div v-else>Loading...</div>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
 // component imports
 import PostList from "../components/PostList.vue";
+import getPosts from "../composables/getPosts";
 
 export default {
   name: "Home",
   components: { PostList },
   setup() {
-    const posts = ref([]);
-    const error = ref(null);
-
-    const load = async () => {
-      try {
-        let data = await fetch("http://localhost:3000/posts");
-        if (!data.ok) {
-          throw Error("no data available");
-        }
-        posts.value = await data.json();
-        console.log(posts.value);
-      } catch (err) {
-        error.value = err.message;
-        console.log(error.value);
-      }
-    };
-
+    // 'composable function'
+    // on the beginning empty, than filled
+    const { posts, error, load } = getPosts();
     load();
-
     return { posts, error };
   },
 };
