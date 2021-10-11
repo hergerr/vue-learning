8cc1a07df6e4818514a38b9583bff8182a0b6993
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2021-10-11 12:02:43 +0200

    Added sorting

--- job-board/src/App.vue
+++ job-board/src/App.vue
@@ -1,16 +1,25 @@
 <template>
   <div class="app">
-    <p>{{ jobs[0].location }}</p>
+    <header>
+      <div class="order">
+        <button @click="handleClick('title')">order by title</button>
+        <button @click="handleClick('salary')">order by salary</button>
+        <button @click="handleClick('location')">order by location</button>
+      </div>
+    </header>
+    <JobList :jobs="jobs" :order="order" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
+import JobList from "./components/JobList.vue";
 import Job from "./types/Job";
+import OrderTerm from "./types/OrderTerm";
 
 export default defineComponent({
   name: "App",
-  components: {},
+  components: { JobList },
   setup() {
     const jobs = ref<Job[]>([
       {
@@ -39,10 +48,32 @@ export default defineComponent({
         id: "5",
       },
     ]);
-    return { jobs };
+    const order = ref<OrderTerm>("title");
+
+    const handleClick = (term: OrderTerm) => {
+      order.value = term;
+    };
+
+    return { jobs, handleClick, order };
   },
 });
 </script>
 
 <style>
+header {
+  text-align: center;
+}
+header .order {
+  margin-top: 20px;
+}
+button {
+  margin: 0 10px;
+  color: #1195c9;
+  border: 3px solid #1195c9;
+  background: #d5f0ff;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
 </style>
---
+++ job-board/src/types/OrderTerm.ts
@@ -0,0 +1,4 @@
+// 3 string values possible
+type OrderTerm = "location" | "title" | "salary";
+
+export default OrderTerm;
