1ecadec6dedf2106e0084ce3e3b7b8ce0c857f26
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2021-10-11 12:17:09 +0200

    Added animation

--- job-board/src/components/JobList.vue
+++ job-board/src/components/JobList.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="job-list">
     <p>Ordered by {{ order }}</p>
-    <ul>
+    <!-- moving cards while sorting -->
+    <transition-group name="list" tag="ul">
       <li v-for="job in orderedJobs" :key="job.id">
         <h2>{{ job.title }} in {{ job.location }}</h2>
         <div class="salary">
@@ -16,7 +17,7 @@
           </p>
         </div>
       </li>
-    </ul>
+    </transition-group>
   </div>
 </template>
 <script lang="ts">
@@ -76,5 +77,8 @@ export default defineComponent({
   color: #17bf66;
   font-weight: bold;
   margin: 10px 4px;
+}
+.list-move {
+  transition: all 1s;
 }
 </style>
